005a35e1af4dc4f3889e0e7478e997513bb85e04
Manuel Wetzel <manuel.wetzel@dlr.de>
2023-12-06 14:39:09 +0100

    parsing with nom

--- day-06/src/bin/part1.rs
+++ day-06/src/bin/part1.rs
@@ -1,22 +1,40 @@
 use std::fs;
 use itertools::Itertools;
 use regex::Regex;
-use nom::multi::{separated_list1, separated_list0};
-use nom::bytes::complete::tag;
-use nom::character::complete::{newline,space0,u32};
-use nom::sequence::{delimited,pair};
+use nom::{multi::{separated_list1, separated_list0, many0},
+          bytes::complete::{tag,take_until,is_not},
+          character::complete::{newline,space0,space1,digit1},
+          sequence::{delimited,pair,terminated,tuple},
+          IResult,
+          combinator::{map_res}
+};
 
 fn read_input() -> String {
-    let input: String = fs::read_to_string("src/bin/input.txt").expect("Expected to read the file");
+    let input: String = fs::read_to_string("src/bin/input.txt").expect("Expected input.txt");
     return input;
 }
 
+fn parse_numbers(input: &str) -> IResult<&str, u32> {
+    return map_res(digit1, |s: &str| s.parse::<u32>())(input)
+}
+
+fn parse_list(input: &str) -> IResult<&str, Vec<u32>> {
+    return separated_list1(space1, parse_numbers)(input)
+}
+
+fn parse_line(input: &str) -> IResult<&str, (&str, &str, &str, Vec<u32>)> {
+    return tuple((is_not(":"), tag(":"), space1, parse_list))(input)
+}
+
+fn parse_input(input: &str) -> Vec<Vec<u32>> {
+    return input.lines().map(|x| parse_line(x).unwrap().1.3).collect_vec()
+}
+
 fn run(input: String) -> i32 {
-    // let mut parse_time = pair(tag("Time:"), separated_list0(tag(" "), u32));
-    // let mut parse_dist = pair(tag("Distance:"), separated_list0(tag(" "), u32));
-    let mut li = input.lines().into_iter();
-    let time: Vec<i32> = li.next().unwrap().split(":").last().unwrap().split(" ").filter(|x| x.len() > 0).map(|x| x.parse().unwrap()).collect_vec();
-    let dist: Vec<i32> = li.next().unwrap().split(":").last().unwrap().split(" ").filter(|x| x.len() > 0).map(|x| x.parse().unwrap()).collect_vec();
+    let matrix = parse_input(input.as_str());
+    let time = &matrix[0];
+    let dist = &matrix[1];
+
     let mut race_mult = 1;
     for (i, j) in time.iter().zip(dist.iter()) {
         let mut races = 0;
@@ -27,7 +45,6 @@ fn run(input: String) -> i32 {
             }
         }
         race_mult = race_mult * races;
-        println!("{}", races)
     }
     return race_mult
 }
@@ -45,7 +62,7 @@ mod tests {
     #[test]
     fn test() {
         let input: &'static str = "Time:      7  15   30
-Distance:  9  40  200";
+        Distance:  9  40  200";
         assert_eq!(run(input.to_string()), 288);
     }
 }
